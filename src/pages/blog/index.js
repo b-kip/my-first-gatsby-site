b507eb6d636b166cd61637d0f08a931191c27608
b-kip <bruzkip@gmail.com>
2023-04-18 09:32:05 +0300

    feat(blog): Finished Gatsby tutorial part 6

---
+++ src/pages/blog/index.js
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const BlogPage = ({ data }) => {
+	console.log(data)
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug1}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))
+      }
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query {
+	allMdx(sort: { frontmatter: { date: DESC }}) {
+		nodes {
+			frontmatter {
+				date(formatString: "MMMM D, YYYY")
+				title
+				slug1
+				test
+			}
+			id
+		}
+	}
+}
+`
+
+export const Head = () => <Seo title="My Blog Posts" />
+
+export default BlogPage
